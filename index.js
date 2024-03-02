97c7fffdce36c714018c71de4ba4f27e4f977a5f
teb <85996160+Teebak@users.noreply.github.com>
2024-03-02 23:01:46 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainNode = document.getElementById('main');
+mainNode.parentNode.removeChild(mainNode);
+
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = `teb is the champion`
+ocument.body.appendChild(newHeader);
